37c04b0f606d9b2d739e44a5ce21215cb91ff672
NaginCarferC <140028251+NaginCarferC@users.noreply.github.com>
2024-07-26 20:16:20 -0500

    Update MenuPanel.java

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -16,7 +16,7 @@ public class MenuPanel extends JPanel {
 
         //Creando el boton de inicio
         JButton startButton = new JButton("Start Game");
-        startButton.setBackground(new Color(35, 144, 137));
+        startButton.setBackground(new Color(255, 220, 180));
         startButton.setForeground(Color.WHITE);
         startButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
