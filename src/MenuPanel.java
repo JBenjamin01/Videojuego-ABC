ad4a77843a0f1afcd8ace20d3c0a1a2aed168240
Antony <acondoricai@unsa.edu.pe>
2024-07-26 20:54:18 -0500

    Corrigiendo error

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+
 
 public class MenuPanel extends JPanel {
 
