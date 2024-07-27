aac62e15fd9a0b1d69d79dcc4bfc8f6c8a64c749
NaginCarferC <140028251+NaginCarferC@users.noreply.github.com>
2024-07-27 16:43:01 -0500

    Update CharacterSelectionPanel.java

--- src/CharacterSelectionPanel.java
+++ src/CharacterSelectionPanel.java
@@ -5,28 +5,23 @@ import java.awt.event.ActionListener;
 import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.ImageIcon;
-import java.io.IOException;
 
 public class CharacterSelectionPanel extends JPanel {
     private Image fondo;
 
     public CharacterSelectionPanel(Game game) {
-        
-        fondo = new ImageIcon(getClass().getResource("imagenes/fondo.jpg")).getImage();
+        // Añado la imagen de fondo
+        fondo = new ImageIcon(getClass().getResource("/imagenes/fondo.jpg")).getImage();
 
-        // botones de personaje
-        try {
-            this.add(createCharacterButton(game, "imagenes/stitch.png", "Stitch"));
-            this.add(createCharacterButton(game, "imagenes/pooh.png", "Pooh"));
-            this.add(createCharacterButton(game, "imagenes/dalmata.png", "Dalmata"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Crear y añadir los botones de personaje con imágenes redimensionadas
+        this.add(createCharacterButton(game, "imagenes/stitch.com (2).png", "Stitch"));
+        this.add(createCharacterButton(game, "imagenes/pooh.png", "Pooh"));
+        this.add(createCharacterButton(game, "imagenes/dalmata.png", "Dalmata"));
     }
 
-    private JButton createCharacterButton(Game game, String imagePath, String characterName) throws IOException {
-        ImageIcon icon = new ImageIcon(imagePath);
-        Image image = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH); // tamaño de la imagen 
+    private JButton createCharacterButton(Game game, String imagePath, String characterName) {
+        ImageIcon icon = new ImageIcon(getClass().getResource(imagePath));
+        Image image = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH); 
         JButton button = new JButton(new ImageIcon(image));
         button.addActionListener(new ActionListener() {
             @Override
@@ -43,4 +38,5 @@ public class CharacterSelectionPanel extends JPanel {
         g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
     }
 }
+
 
