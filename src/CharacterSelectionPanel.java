6c9bee8497e19b78d382a560fcf21d56ed814510
NaginCarferC <140028251+NaginCarferC@users.noreply.github.com>
2024-07-27 18:22:09 -0500

    Update CharacterSelectionPanel.java

--- src/CharacterSelectionPanel.java
+++ src/CharacterSelectionPanel.java
@@ -5,6 +5,9 @@ import java.awt.event.ActionListener;
 import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.io.File;
 
 public class CharacterSelectionPanel extends JPanel {
     private Image fondo;
@@ -12,6 +15,13 @@ public class CharacterSelectionPanel extends JPanel {
     public CharacterSelectionPanel(Game game) {
         // Añado la imagen de fondo
         fondo = new ImageIcon(getClass().getResource("/imagenes/fondo.jpg")).getImage();
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = GridBagConstraints.RELATIVE;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new java.awt.Insets(10, 10, 10, 10);
 
         // Crear y añadir los botones de personaje con imágenes redimensionadas
         this.add(createCharacterButton(game, "imagenes/stitch.png", "Stitch"));
@@ -21,8 +31,9 @@ public class CharacterSelectionPanel extends JPanel {
 
     private JButton createCharacterButton(Game game, String imagePath, String characterName) {
         ImageIcon icon = new ImageIcon(getClass().getResource(imagePath));
-        Image image = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH); 
+        Image image = icon.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH); 
         JButton button = new JButton(new ImageIcon(image));
+        button.setPreferredSize(new java.awt.Dimension(160, 160));
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
