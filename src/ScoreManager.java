b92ac75472c58ef567b2351fa9c480862f42edd0
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-07-28 14:19:31 -0500

    Clase para el puntaje

---
+++ src/ScoreManager.java
@@ -0,0 +1,27 @@
+public class ScoreManager {
+    private static ScoreManager instance;
+    private int score;
+
+    private ScoreManager() {
+        score = 0;
+    }
+
+    public static ScoreManager getInstance() {
+        if (instance == null) {
+            instance = new ScoreManager();
+        }
+        return instance;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void increaseScore(int points) {
+        score += points;
+    }
+
+    public void resetScore() {
+        score = 0;
+    }
+}
