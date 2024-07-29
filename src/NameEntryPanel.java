576a9bd34169834576992588166517ab12a21247
Jhonatan Benjamin Mamani Céspedes <jmamanices@unsa.edu.pe>
2024-07-28 20:27:15 -0500

    Mejorando entrada del nombre

--- src/NameEntryPanel.java
+++ src/NameEntryPanel.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class NameEntryPanel extends JPanel {
-    private Image fondo;
     private JTextField nameField;
     private JButton startButton;
     private Game game;
@@ -13,30 +12,33 @@ public class NameEntryPanel extends JPanel {
 
     public NameEntryPanel(Game game) {
         this.game = game;
-        fondo = new ImageIcon(getClass().getResource("/imagenes/menu.gif")).getImage();
         setLayout(new GridBagLayout());
-        setOpaque(false);
+        setOpaque(true);
+        setBackground(new Color(211, 211, 211));
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
         gbc.anchor = GridBagConstraints.CENTER;
 
         JPanel inputPanel = new JPanel();
-        inputPanel.setOpaque(false);
+        inputPanel.setOpaque(true);
+        inputPanel.setBackground(new Color(211, 211, 211));
         inputPanel.setLayout(new GridBagLayout());
+        inputPanel.setPreferredSize(new Dimension(500, 280));
         GridBagConstraints inputGbc = new GridBagConstraints();
         inputGbc.insets = new Insets(5, 5, 20, 5);
         inputGbc.anchor = GridBagConstraints.CENTER;
 
         JLabel nameLabel = new JLabel("Ingresa tu nombre:");
-        nameLabel.setFont(new Font("Cooper Black", Font.PLAIN, 70));
+        nameLabel.setFont(new Font("Cooper Black", Font.PLAIN, 50));
         nameLabel.setForeground(Color.BLACK);
         inputGbc.gridx = 0;
         inputGbc.gridy = 0;
         inputPanel.add(nameLabel, inputGbc);
 
-        nameField = new JTextField(30);
-        nameField.setFont(new Font("Serif", Font.PLAIN, 45));
+        nameField = new JTextField(20);
+        nameField.setFont(new Font("Serif", Font.PLAIN, 30));
+        nameField.setPreferredSize(new Dimension(350, 50));
         inputGbc.gridy = 1;
         inputPanel.add(nameField, inputGbc);
 
@@ -46,7 +48,7 @@ public class NameEntryPanel extends JPanel {
         startButton.setBackground(new Color(0, 102, 204));
         startButton.setForeground(Color.WHITE);
 
-        Border border = BorderFactory.createLineBorder(Color.BLACK, 5);
+        Border border = BorderFactory.createLineBorder(Color.BLACK, 2); // Bordes más delgados
         Border raisedBevelBorder = BorderFactory.createRaisedBevelBorder();
         startButton.setBorder(BorderFactory.createCompoundBorder(border, raisedBevelBorder));
 
@@ -80,8 +82,5 @@ public class NameEntryPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (fondo != null) {
-            g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
-        }
     }
 }
